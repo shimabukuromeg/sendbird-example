ab9896b96f559eeadacbf7a8c9f885c8f69be34f
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-01-24 00:13:45 +0900

    format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from 'next';
 import 'sendbird-uikit/dist/index.css';
 import dynamic from 'next/dynamic';
 
-const DynamicAppWithNoSSR = dynamic(() => import("../components/Chat/Chat"), {
+const DynamicAppWithNoSSR = dynamic(() => import('../components/Chat/Chat'), {
   ssr: false,
-  loading: () => <p>...</p>
+  loading: () => <p>...</p>,
 });
 
-const Home: NextPage = () => (<DynamicAppWithNoSSR />);
+const Home: NextPage = () => <DynamicAppWithNoSSR />;
 
 export default Home;
